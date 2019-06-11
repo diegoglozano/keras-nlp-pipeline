d782c179a36175fb97166ca2f0ebfdf775f1b015
Diego Garcia Lozano <dgl_1096@hotmail.com>
2019-06-11 18:18:43 +0200

    Adapting TokenizerTransformer to inherit from Tokenizer

--- library_pipeline_nlp/my_transformers.py
+++ library_pipeline_nlp/my_transformers.py
@@ -4,17 +4,17 @@ from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 
 
-class TokenizerTransformer(BaseEstimator, TransformerMixin):
+class TokenizerTransformer(BaseEstimator, TransformerMixin, Tokenizer):
 
-    def __init__(self):
-        self.tokenizer = Tokenizer()
+    def __init__(self, **tokenizer_params):
+        Tokenizer.__init__(self, **tokenizer_params)
 
     def fit(self, X, y=None):
-        self.tokenizer.fit_on_texts(X)
+        self.fit_on_texts(X)
         return self
 
     def transform(self, X, y=None):
-        X_transformed = self.tokenizer.texts_to_sequences(X)
+        X_transformed = self.texts_to_sequences(X)
         return X_transformed
 
 
@@ -29,18 +29,4 @@ class PadSequencesTransformer(BaseEstimator, TransformerMixin):
     def transform(self, X, y=None):
         X_padded = pad_sequences(X, maxlen=self.maxlen)
         return X_padded
-
-
-class TokenizerTransformerInherit(BaseEstimator, TransformerMixin, Tokenizer):
-
-    def __init__(self, **tokenizer_params):
-        Tokenizer.__init__(self, **tokenizer_params)
-
-    def fit(self, X, y=None):
-        self.fit_on_texts(X)
-        return self
-
-    def transform(self, X, y=None):
-        X_transformed = self.texts_to_sequences(X)
-        return X_transformed
 
